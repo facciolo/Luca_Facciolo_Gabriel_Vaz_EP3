93ed4df0dfd4ba94ead1a423d34378c21e53ed25
facciolo <luca@siryus.com.br>
2015-04-20 17:27:59 -0300

    3
    separando funçoes do programa e desenvolvendo-o

--- dieta.py
+++ dieta.py
@@ -1,3 +1,21 @@
+import funçoesdieta
+def alimentodia(alimento):#função que insere os alimentos em seus respectivos dias
+    global dia1,dia2,dia3,dia4,dia5,dia6,dia7
+    if alimento[0:5] == '06/04':
+        dia1+=alimento+'\n'
+    elif alimento[0:5] == '07/04':
+        dia2+=alimento+'\n'
+    elif alimento[0:5] == '08/04':
+        dia3+=alimento+'\n'
+    elif alimento[0:5] == '09/04':
+        dia4+=alimento+'\n'
+    elif alimento[0:5] == '10/04':
+        dia5+=alimento+'\n'
+    elif alimento[0:5] == '11/04':
+        dia6+=alimento+'\n'
+    elif alimento[0:5] == '12/04':
+        dia7+=alimento+'\n'
+        
 TMB = 0
 lista_alimentos = open("alimentos.csv","r+",encoding = "utf-8")
 pessoa = open("usuário.csv","r+",encoding = "utf-8")
@@ -12,8 +30,30 @@ alimento_1=lista_dias[0]
 alimento_2=lista_dias[1]
 alimento_3=lista_dias[2]
 alimento_4=lista_dias[3]
+alimento_5=lista_dias[4]
+alimento_6=lista_dias[5]
+alimento_7=lista_dias[6]
+alimento_8=lista_dias[7]
 
-print(alimento_1[0:5])
+
+dia1 = []
+dia2 = []
+dia3 = []
+dia4 = []
+dia5 = []
+dia6 = []
+dia7 = []
+alimentodia(alimento_1)
+alimentodia(alimento_2)
+alimentodia(alimento_3)
+alimentodia(alimento_4)
+alimentodia(alimento_5)
+alimentodia(alimento_6)
+alimentodia(alimento_7)
+alimentodia(alimento_8)
+
+print(''.join(dia1))
+print(''.join(dia2))
 #(linhas 10-15) Definição dos parametros pessoais utilizados 
 nome = lista[0]
 idade = int(lista[1])
@@ -30,36 +70,8 @@ print(dados_alimentospessoa)
 #print(alimentos2)
 
 
-def FGBenedict():#função que define a taxa metábolica
-#OBS: peso em kilos , altura em cm e idade em anos
-    if sexo == "M":
-        TMB = 88.36 + (13.4*peso) + (4.8*altura) - (5.7*idade)
-    else:
-        TMB = 447.6 + (9.2*peso) + (3.1*altura) - (4.3*idade)
-    return TMB
+
     
-TMB = FGBenedict()
-        
-def grau_atividade():
-    global TMB
-    if fator == "mínimo":
-        TMB = TMB*1.2
-    elif fator == "baixo":
-        TMB = TMB*1.375
-    elif fator == "médio":
-        TMB = TMB*1.55
-    elif fator == "alto":
-        TMB = TMB*1.725
-    elif fator == "muito alto":
-        TMB = TMB*1.9
-    return TMB
-grau_atividade()
+TMB = funçoesdieta.FGBenedict(sexo,peso,altura,idade)
+TMB = funçoesdieta.grau_atividade(TMB,fator)
 print(TMB)
-
-
-    
-    
-    
-    
-    
-        
