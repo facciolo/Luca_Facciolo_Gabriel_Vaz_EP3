2292c0ce932cc28c31f3b62d215d506be690df72
facciolo <luca@siryus.com.br>
2015-04-22 21:41:30 -0300

    sexta vez - organização e refinamento

--- dieta.py
+++ dieta.py
@@ -1,24 +1,5 @@
 from funçoesdieta import *
 
-def alimentodia(alimento):#função que insere os alimentos em seus respectivos dias
-    global dia1,dia2,dia3,dia4,dia5,dia6,dia7
-    if alimento[0:5] == '06/04':
-        dia1+=alimento+ '\n'
-    elif alimento[0:5] == '07/04':
-        dia2+=alimento+ '\n'
-    elif alimento[0:5] == '08/04':
-        dia3+=alimento+ '\n'
-    elif alimento[0:5] == '09/04':
-        dia4+=alimento+ '\n'
-    elif alimento[0:5] == '10/04':
-        dia5+=alimento+ '\n'
-    elif alimento[0:5] == '11/04':
-        dia6+=alimento+ '\n'
-    elif alimento[0:5] == '12/04':
-        dia7+=alimento+ '\n'
-    return dia1,dia2,dia3,dia4,dia5,dia6,dia7    
-        
-        
 alimentos = open("alimentos.csv","r+",encoding = "utf-8")
 ficha_pessoa = open("usuário.csv","r+",encoding = "utf-8")
 ficha_pessoa = ficha_pessoa.read()
@@ -52,7 +33,8 @@ altura = float(dados_pessoa[4])
 fator = dados_pessoa[5]
 
 
-'''print(dados_pessoa)
+'''TESTE
+print(dados_pessoa)
 print("TESTE")
 print(dieta)
 print(alimentos)
@@ -63,18 +45,59 @@ print(IMC)
 
 
 
-#atribuindo valores da taxa metabolica    
+#atribuindo valores da taxa metabolica e IMC  
 TMB = FGBenedict(sexo,peso,altura,idade)
 TMB = grau_atividade(TMB,fator)
 IMC = IMC(peso,altura)
 
 #inserção de alimento nas listas dos dias 
 for alimento in lista_dias:
-    alimentodia(alimento)
-print(''.join(dia1))
-print(''.join(dia2))
+    if alimento[0:5] == '06/04':
+        dia1+=alimento + ','
+    elif alimento[0:5] == '07/04':
+        dia2+=alimento+ ','
+    elif alimento[0:5] == '08/04':
+        dia3+=alimento+ ','
+    elif alimento[0:5] == '09/04':
+        dia4+=alimento+ ','
+    elif alimento[0:5] == '10/04':
+        dia5+=alimento+ ','
+    elif alimento[0:5] == '11/04':
+        dia6+=alimento+ ','
+    elif alimento[0:5] == '12/04':
+        dia7+=alimento+ ','
+    
+    
+dia1 = ('').join(dia1)
+dia2 = ('').join(dia2)
+dia3 = ('').join(dia3)
+dia4 = ('').join(dia4)
+dia5 = ('').join(dia5)
+dia6 = ('').join(dia6)
+dia7 = ('').join(dia7)
 
 
-#compara_alimento(lista_dias,alimentos)#deveria retornar todos valores(calorias,proteinas e gorduras)
+dia1 = dia1.split(',')
+
+'''print(dia1)
+print(dia1[1:13:3])
+print(dia1[2:13:3])'''
+    
+
+
+#Cria um catalogo de todos alimentos 
 catalogo_alimentos(catalogo,alimentos)
-print(catalogo)
+
+for i in range(1,13,3):#primeiro dia
+    b = (float(catalogo[dia1[i]][1])*float((dia1[i+1]))/100)#separa apenas calorias
+    #codigo que soma cada membro de b
+    c = (float(catalogo[dia1[i]][2])*float((dia1[i+1]))/100)#separa apenas proteinas
+    #codigo que soma cada membro de b
+    d = (float(catalogo[dia1[i]][3])*float((dia1[i+1]))/100)#separa apenas carboidratos
+    #codigo que soma cada membro de b
+    e = (float(catalogo[dia1[i]][4])*float((dia1[i+1]))/100)#separa apenas gorduras
+    
+
+
+    
+        
--- funçoesdieta.py
+++ funçoesdieta.py
@@ -1,10 +1,4 @@
-dia1 = []
-dia2 = []
-dia3 = []
-dia4 = []
-dia5 = []
-dia6 = []
-dia7 = []
+
 def FGBenedict(sexo,peso,altura,idade):#função que define a taxa metábolica
 #OBS: peso em kilos , altura em cm e idade em anos
     if sexo == "M":
@@ -24,36 +18,12 @@ def grau_atividade(TMB,fator):#função que define a taxa metábolica de acordo 
         TMB = TMB*1.725
     elif fator == "muito alto":
         TMB = TMB*1.9
-    return TMB
-def alimentodia(alimento):#função que insere os alimentos em seus respectivos dias
-    global dia1,dia2,dia3,dia4,dia5,dia6,dia7
-    if alimento[0:5] == '06/04':
-        dia1+=alimento+'\n'
-    elif alimento[0:5] == '07/04':
-        dia2+=alimento+'\n'
-    elif alimento[0:5] == '08/04':
-        dia3+=alimento+'\n'
-    elif alimento[0:5] == '09/04':
-        dia4+=alimento+'\n'
-    elif alimento[0:5] == '10/04':
-        dia5+=alimento+'\n'
-    elif alimento[0:5] == '11/04':
-        dia6+=alimento+'\n'
-    elif alimento[0:5] == '12/04':
-        dia7+=alimento+'\n'
-    return dia1,dia2,dia3,dia4,dia5,dia6,dia7
-def compara_alimento(lista_dias,alimentos):
-    for alimento in lista_dias:
-        None
-    for alimento2 in alimentos:
-        None
-    if alimento[0:5] == alimento2[0:5]:
-        return alimento2
-
+    return TMB 
+    
 def IMC(peso,altura):
     return (1.3*peso)/(altura**2.5)
 
-def catalogo_alimentos(catalogo,alimentos):
+def catalogo_alimentos(catalogo,alimentos):#cria um dicionario com todos elementos do arquivo alimentos
     for termo in alimentos:
         valores = []
         linha = []
@@ -62,5 +32,3 @@ def catalogo_alimentos(catalogo,alimentos):
             valores.append(float(numero))
         catalogo[linha[0]] = valores
     return catalogo
-    
-    
